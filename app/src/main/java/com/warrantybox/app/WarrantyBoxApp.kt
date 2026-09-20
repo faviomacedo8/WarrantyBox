1a2d353dc6b5b5b7f314ec04626c6d216b819eaa
faviomacedo8 <faviomacedo8@gmail.com>
2026-09-20 17:51:43 +0200

    Ligar repositório à base de dados transacional

--- app/src/main/java/com/warrantybox/app/WarrantyBoxApp.kt
+++ app/src/main/java/com/warrantybox/app/WarrantyBoxApp.kt
@@ -1,4 +1,9 @@
 package com.warrantybox.app
+
 import android.app.Application
 import com.warrantybox.app.data.*
-class WarrantyBoxApp:Application(){val database by lazy{AppDatabase.get(this)};val repository by lazy{WarrantyRepository(this,database.dao())}}
+
+class WarrantyBoxApp : Application() {
+    val database by lazy { AppDatabase.get(this) }
+    val repository by lazy { WarrantyRepository(this, database.dao(), database) }
+}
